8de7ba2b4cbc42e25b9583d55f4221b7c4c43289
anonymous <avvinay1@outlook.com>
2024-08-11 00:01:50 +0530

    initial version

--- Webapp/obj/Debug/net8.0/Webapp.AssemblyInfo.cs
+++ Webapp/obj/Debug/net8.0/Webapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a70c82330ddeb60c2af46e54ffd0e664d18b5960")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
