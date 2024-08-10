a70c82330ddeb60c2af46e54ffd0e664d18b5960
anonymous <avvinay1@outlook.com>
2024-08-10 23:18:52 +0530

    web app initial version

---
+++ WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace Sample_Project
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
